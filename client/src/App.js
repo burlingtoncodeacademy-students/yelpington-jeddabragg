77e265635545faf8632160c3910d69afc807a44a
Jedda Bragg <jeddabragg@gmail.com>
2021-11-08 09:05:05 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Map from './components/Map.js';
 import './App.css';
 
+
+
 function App() {
+const [center, setCenter] = useState([44.4759, -73.2121])
   return (
+    <>
     <h1>Yelpington</h1>
+    <Map center={center} setCenter={setCenter}/>
+    </>
   );
 }
 
 export default App;
+
